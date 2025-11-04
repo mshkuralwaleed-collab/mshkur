41c72e17c9e08c34bcb45ffeae1ed0fb703c492d
mshkur alwaleed <mshkuralwaleed@gmail.com>
2025-11-04 23:50:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2374334333-21539",
+  "appId": "1:284620889447:web:2428ee6a09903feff6a3c2",
+  "apiKey": "AIzaSyDxhq1a_xR6Ez3zMk8Y9GTzT0tN2he9um4",
+  "authDomain": "studio-2374334333-21539.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "284620889447"
+};
